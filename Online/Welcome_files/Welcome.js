5d4b93e44afcfa42a591fcfd5f60c6ca84cb06e9
man-of-the-east <rhodedijinidald@gmail.com>
2015-12-01 18:36:08 -0500

    Big change to entire site, finished look
    12/1 6:36pm

--- Online/Welcome_files/Welcome.js
+++ Online/Welcome_files/Welcome.js
@@ -1,9 +1,9 @@
 // Created by iWeb 3.0.4 local-build-20151201
 
 setTransparentGifURL('Media/transparent.gif');function applyEffects()
-{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_1:new IWPhotoFrame([IWCreateImage('Welcome_files/GraphPaper_01.png'),IWCreateImage('Welcome_files/GraphPaper_02.png'),IWCreateImage('Welcome_files/GraphPaper_01_1.png'),IWCreateImage('Welcome_files/GraphPaper_01_2.png'),IWCreateImage('Welcome_files/GraphPaper_09.png'),IWCreateImage('Welcome_files/GraphPaper_08.png'),IWCreateImage('Welcome_files/GraphPaper_07.png'),IWCreateImage('Welcome_files/GraphPaper_01_3.png')],null,0,0.600000,90.000000,0.000000,90.000000,0.000000,89.000000,11.000000,89.000000,7.000000,11.000000,11.000000,11.000000,11.000000,'Welcome_files/graphpaper_tape.png',new IWPoint(0.500000,0),new IWSize(155,47),0.300000),stroke_0:new IWPhotoFrame([IWCreateImage('Welcome_files/Modern_C_TL.png'),IWCreateImage('Welcome_files/Modern_S_T.png'),IWCreateImage('Welcome_files/Modern_C_TR.png'),IWCreateImage('Welcome_files/Modern_S_R.png'),IWCreateImage('Welcome_files/Modern_C_BR.png'),IWCreateImage('Welcome_files/Modern_S_B.png'),IWCreateImage('Welcome_files/Modern_C_BL.png'),IWCreateImage('Welcome_files/Modern_S_L.png')],null,2,0.700000,0.000000,0.000000,0.000000,0.000000,17.000000,17.000000,17.000000,20.000000,40.000000,40.000000,40.000000,240.000000,null,null,null,0.100000)});registry.applyEffects();}
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWEmptyStroke()});registry.applyEffects();}
 function hostedOnDM()
 {return false;}
 function onPageLoad()
 {loadMozillaCSS('Welcome_files/WelcomeMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');IMpreload('Welcome_files','shapeimage_3','0');IMpreload('Welcome_files','shapeimage_4','0');applyEffects()}
+fixAllIEPNGs('Media/transparent.gif');IMpreload('Welcome_files','shapeimage_3','0');IMpreload('Welcome_files','shapeimage_4','0');applyEffects()}
