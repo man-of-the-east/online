60d264a85a48c849f22f8b55eaac4c0a2d416ded
man-of-the-east <rhodedijinidald@gmail.com>
2015-12-01 17:36:41 -0500

    Changes to Blog Page
    12/1 5:36 pm, just seeing how this will look

--- Online/Blog/Blog_files/Blog.js
+++ Online/Blog/Blog_files/Blog.js
@@ -1,13 +1,13 @@
 // Created by iWeb 3.0.4 local-build-20151201
 
 setTransparentGifURL('../Media/transparent.gif');function applyEffects()
-{var registry=IWCreateEffectRegistry();registry.registerEffects({reflection_0:new IWReflection({opacity:0.50,offset:1.00})});registry.applyEffects();}
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWEmptyStroke()});registry.applyEffects();}
 function hostedOnDM()
 {return false;}
 function photocastSubscribe()
 {photocastHelper("file://localhost/Users/joshuaeastman/Desktop/online/Online/Blog/rss.xml");}
 function onPageLoad()
 {loadMozillaCSS('Blog_files/BlogMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');detectBrowser();adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');fixAllIEPNGs('../Media/transparent.gif');Widget.onload();fixupAllIEPNGBGs();applyEffects()}
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');detectBrowser();adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('../Media/transparent.gif');applyEffects()}
 function onPageUnload()
 {Widget.onunload();}
