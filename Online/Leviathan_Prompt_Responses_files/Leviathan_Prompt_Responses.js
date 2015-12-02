9b09fd0523ce22bddedf95f9aef420f0fabbc9e5
man-of-the-east <rhodedijinidald@gmail.com>
2015-12-01 21:14:16 -0500

    Adding some English Work to site
    12/1 9:14pm

---
+++ Online/Leviathan_Prompt_Responses_files/Leviathan_Prompt_Responses.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.4 local-build-20151201
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Leviathan_Prompt_Responses_files/Leviathan_Prompt_ResponsesMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
