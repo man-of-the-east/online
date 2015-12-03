f621635640a026128d5fbd114ca99333859167c8
man-of-the-east <rhodedijinidald@gmail.com>
2015-12-02 21:58:32 -0500

    12/2 9:58
    Daily Blog

--- Online/Blog/Blog_files/Blog.js
+++ Online/Blog/Blog_files/Blog.js
@@ -1,13 +1,11 @@
-// Created by iWeb 3.0.4 local-build-20151201
+// Created by iWeb 3.0.4 local-build-20151202
 
-setTransparentGifURL('../Media/transparent.gif');function applyEffects()
-{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWEmptyStroke()});registry.applyEffects();}
-function hostedOnDM()
+setTransparentGifURL('../Media/transparent.gif');function hostedOnDM()
 {return false;}
 function photocastSubscribe()
 {photocastHelper("file://localhost/Users/joshuaeastman/Desktop/online/Online/Blog/rss.xml");}
 function onPageLoad()
 {loadMozillaCSS('Blog_files/BlogMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');detectBrowser();adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('../Media/transparent.gif');applyEffects()}
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');detectBrowser();adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('../Media/transparent.gif');performPostEffectsFixups()}
 function onPageUnload()
 {Widget.onunload();}
--- Online/Welcome_files/Welcome.js
+++ Online/Welcome_files/Welcome.js
@@ -1,4 +1,4 @@
-// Created by iWeb 3.0.4 local-build-20151201
+// Created by iWeb 3.0.4 local-build-20151202
 
 setTransparentGifURL('Media/transparent.gif');function applyEffects()
 {var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWEmptyStroke()});registry.applyEffects();}
@@ -6,4 +6,4 @@ function hostedOnDM()
 {return false;}
 function onPageLoad()
 {loadMozillaCSS('Welcome_files/WelcomeMoz.css')
-fixAllIEPNGs('Media/transparent.gif');IMpreload('Welcome_files','shapeimage_3','0');IMpreload('Welcome_files','shapeimage_4','0');applyEffects()}
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');fixAllIEPNGs('Media/transparent.gif');IMpreload('Welcome_files','shapeimage_3','0');IMpreload('Welcome_files','shapeimage_4','0');applyEffects()}
