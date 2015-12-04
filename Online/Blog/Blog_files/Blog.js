43b5ce6dc509aa9ee72be1e3b840471b670dba9e
man-of-the-east <rhodedijinidald@gmail.com>
2015-12-04 16:37:57 -0500

    12/3 4:37pm
    Added “Facepalm” Blog

--- Online/Blog/Blog_files/Blog.js
+++ Online/Blog/Blog_files/Blog.js
@@ -1,4 +1,4 @@
-// Created by iWeb 3.0.4 local-build-20151202
+// Created by iWeb 3.0.4 local-build-20151204
 
 setTransparentGifURL('../Media/transparent.gif');function hostedOnDM()
 {return false;}
