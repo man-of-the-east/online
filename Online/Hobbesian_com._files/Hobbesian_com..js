a19c54bcf2ac612c051bb321cec4928b11924e65
man-of-the-east <rhodedijinidald@gmail.com>
2015-12-14 09:43:22 -0500

    12/14/15 9:42am
    Added Hobbes Essay

---
+++ Online/Hobbesian_com._files/Hobbesian_com..js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.4 local-build-20151214
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Hobbesian_com._files/Hobbesian_com.Moz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');Widget.onload();fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
