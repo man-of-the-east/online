6109a8d61f395159cabb2be350400ce91f5724ea
man-of-the-east <rhodedijinidald@gmail.com>
2015-12-18 17:19:24 -0500

    Added "Random" Page
    12/18 5:19pm Bittersweet

--- Online/Blog/Blog_files/Blog.js
+++ Online/Blog/Blog_files/Blog.js
@@ -1,4 +1,4 @@
-// Created by iWeb 3.0.4 local-build-20151204
+// Created by iWeb 3.0.4 local-build-20151218
 
 setTransparentGifURL('../Media/transparent.gif');function hostedOnDM()
 {return false;}
---
+++ Online/Random_files/Random.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.4 local-build-20151218
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Random_files/RandomMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');fixAllIEPNGs('Media/transparent.gif');Widget.onload();performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
